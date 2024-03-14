35339a47ad4b4bd76442287c396201de913e7b81
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2024-03-14 13:03:59 +0530

    Time: 20 ms (100.00%), Space: 7.4 MB (16.67%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.c
@@ -0,0 +1,25 @@
+int numSubarraysWithSum(int* nums, int numsSize, int goal){
+        int i = 0, count = 0, res = 0;
+        for (int j = 0; j <numsSize; ++j) {
+            if (nums[j] == 1) {
+                goal--;
+                count = 0;
+            }
+            
+            while (goal == 0 && i <= j) {
+                goal += nums[i];
+                i++;
+                count++;
+                if (i > j - goal + 1)
+                    break;
+            }
+            
+            while (goal < 0) {
+                goal += nums[i];
+                i++;
+            }
+            
+            res += count;
+        }
+        return res;
+}

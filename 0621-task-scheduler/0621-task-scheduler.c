15070789464432600314e5eb0049854cb2d9a88d
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2024-03-19 22:56:00 +0530

    Time: 26 ms (63.16%), Space: 6.5 MB (21.05%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.c
+++ 0621-task-scheduler/0621-task-scheduler.c
@@ -2,11 +2,10 @@ int min(int a, int b) {
     return a < b ? a : b;
 }
 
-void bubbleSort(int *arr, int size) {
+void Sort(int *arr, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -21,16 +20,16 @@ int leastInterval(char* tasks, int tasksSize, int n) {
         frequencies[tasks[i] - 'A']++;
     }
 
-    bubbleSort(frequencies, 26);
+    Sort(frequencies, 26);
 
     int maxFreq = frequencies[25]; 
-    int idleTime = (maxFreq - 1) * n; 
+    int idle = (maxFreq - 1) * n; 
 
-    for (int i = 24; i >= 0 && idleTime > 0; i--) {
-        idleTime -= min(maxFreq - 1, frequencies[i]);
+    for (int i = 24; i >= 0 && idle> 0; i--) {
+        idle-= min(maxFreq - 1, frequencies[i]);
     }
 
-    idleTime = idleTime > 0 ? idleTime : 0;
+    idle = idle > 0 ? idle: 0;
 
-    return tasksSize + idleTime;
+    return tasksSize + idle;
 }

568fc1aee7f0ee3e1d5a7673789327558fce3292
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2024-04-30 20:21:45 +0530

    Time: 5 ms (17.63%), Space: 9.8 MB (33.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count[3]={0};
+        int ind=0;
+        for(int i=0;i<nums.size();i++){
+            count[nums[i]]++;
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<count[i];j++){
+                nums[ind]=i;
+                ind++;
+            }
+            
+        }
+        
+    }
+};

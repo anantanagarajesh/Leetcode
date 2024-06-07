ac970df4cd1488ca1b46094d5213fba315df5ec8
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2024-06-08 00:12:06 +0530

    Time: 194 ms (36.74%), Space: 36.8 MB (93.49%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_set<string> S(dictionary.begin(), dictionary.end());
+        
+        string ans, word;
+        bool isFound=0;
+
+        for (char c: sentence) {
+            if (c!= ' ') {
+                word+=c;
+                if (!isFound && S.count(word)) {
+                    ans+=word;
+                    isFound=1;
+                }
+            } 
+            else {
+                if (!isFound) ans+=word;
+                ans+=' ';
+                word="";
+                isFound=0;
+            }
+        }
+        if (!isFound)
+            ans+=word;
+
+        return ans;
+    }
+};

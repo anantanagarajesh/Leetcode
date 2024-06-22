fb8e32b28cecc102f1e4f84cec0c74162c65abf9
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2024-06-22 19:28:43 +0530

    Time: 3 ms (79.75%), Space: 19.5 MB (62.05%) - LeetHub

---
+++ 3127-make-a-square-with-the-same-color/3127-make-a-square-with-the-same-color.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canMakeSquare(vector<vector<char>>& grid) {
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                if(grid[i][j]=='B' && grid[i][j+1]=='B'&& grid[i+1][j]=='B' && grid[i+1][j+1]=='B' )
+                    return true;
+                if(grid[i][j]=='W' && grid[i][j+1]=='W'&& grid[i+1][j]=='W' && grid[i+1][j+1]=='W' )
+                    return true;
+            }
+        }
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                if(grid[i][j]=='W' && grid[i][j+1]=='B'&& grid[i+1][j]=='B' && grid[i+1][j+1]=='B' )
+                    return true;
+                if(grid[i][j]=='W' && grid[i][j+1]=='W'&& grid[i+1][j]=='W' && grid[i+1][j+1]=='B' )
+                    return true;
+                if(grid[i][j]=='W' && grid[i][j+1]=='W'&& grid[i+1][j]=='B' && grid[i+1][j+1]=='W' )
+                    return true;
+                if(grid[i][j]=='W' && grid[i][j+1]=='B'&& grid[i+1][j]=='W' && grid[i+1][j+1]=='W' )
+                    return true;
+                
+                if(grid[i][j]=='B' && grid[i][j+1]=='W'&& grid[i+1][j]=='W' && grid[i+1][j+1]=='W' )
+                    return true;
+                if(grid[i][j]=='B' && grid[i][j+1]=='B'&& grid[i+1][j]=='B' && grid[i+1][j+1]=='W' )
+                    return true;
+                if(grid[i][j]=='B' && grid[i][j+1]=='B'&& grid[i+1][j]=='W' && grid[i+1][j+1]=='B' )
+                    return true;
+                if(grid[i][j]=='B' && grid[i][j+1]=='W'&& grid[i+1][j]=='B' && grid[i+1][j+1]=='B' )
+                    return true;
+                
+            }
+        }
+        return false;
+    }
+};

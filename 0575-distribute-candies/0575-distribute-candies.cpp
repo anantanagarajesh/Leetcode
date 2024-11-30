32b67e7903bf91fa37ff00031afe4151ae16cf09
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2024-11-30 20:06:06 +0530

    Time: 96 ms (69.48%), Space: 120.1 MB (62.06%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_map<int,int>mp;
+        int n=candyType.size();
+        for(int i=0;i<n;i++){
+            mp[candyType[i]]++;
+        }
+        if(mp.size()>=n/2)
+        {
+            return n/2;
+        }
+        return mp.size();
+    }
+};
